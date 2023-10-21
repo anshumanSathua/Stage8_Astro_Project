962d99dd3ad7a18f665c2b204cbc0283bddc68d6
Plut0r <ogunolaololade@gmail.com>
2023-10-21 13:01:18 +0100

    feat: landing-page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			container: {
+				center: true,
+				padding: '2rem',
+				screens: {
+					sm: '640px',
+					md: '768px',
+					lg: '1024px',
+					xl: '1280px',
+					'2xl': '1300px',
+				},
+			}
+		},
 	},
 	plugins: [],
 }
